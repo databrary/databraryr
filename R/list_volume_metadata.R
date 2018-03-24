748c59c074f2d25db635c564457198fef9ceb1ca
rogilmore <thatrickgilmore@gmail.com>
2018-03-24 10:26:43 -0400

    Add list_volume_metadata()

---
+++ R/list_volume_metadata.R
@@ -0,0 +1,37 @@
+list_volume_metadata <- function(volume = 2, vb = FALSE) {
+
+  # Error-checking
+  if (!(is.numeric(volume))) {
+    stop("Volume must be a number.")
+  }
+  if (volume <= 0) {
+    stop("Volume must be > 0.")
+  }
+
+  # Declare helpers
+  surround_w_quotes <- function(s) {
+    paste0('"', s, '"')
+  }
+
+  make_url_doi <- function(doi) {
+    if (is.character(doi)) {
+      paste0("https://doi.org/", doi)
+    } else {
+      stop("DOI must be character string")
+    }
+  }
+
+  v <- download_containers_records(volume = volume)
+  if (!(is.null(v))){
+    if (write.header) {
+      cat(paste("volume.id", "volume.name", "permission",
+                "doi\n", sep=","))
+    }
+    cat(paste(v$id, surround_w_quotes(v$name[1]), v$owners[1],
+              v$permission,
+              paste0(make_url_doi(v$doi), "\n"), sep = ","))
+  } else {
+    cat(paste0('No data in volume ', volume, "\n."))
+    return(NULL)
+  }
+}
