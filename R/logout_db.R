caa89baf28aa6920484b0c65bcfa2b09d6682ef1
rogilmore <thatrickgilmore@gmail.com>
2018-03-24 12:49:43 -0400

    httr:: to log_out

--- R/logout_db.R
+++ R/logout_db.R
@@ -11,11 +11,10 @@ logout_db <- function(logout.url = "/api/user/logout",
     config_db(vb = vb)
   }
 
-  r <- POST(paste0(databrary.url, logout.url))
-
+  r <- httr::POST(paste0(databrary.url, logout.url))
   if (status_code(r) == 200){
-    if (vb) cat( 'Logout Successful.\n' )
+    if (vb) cat('Logout Successful.\n')
     if (file.exists(".databrary.RData")) file.remove(".databrary.RData")
-  } else if (vb) cat( paste('Logout Failed, HTTP status ', status_code(r), '\n', sep="" ))
+  } else if (vb) cat( paste('Logout Failed, HTTP status ', httr::status_code(r), '\n', sep="" ))
   if (return.response) return(r)
 }
