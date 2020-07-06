0b32d826badc15b4c3a99350f8a4664be0ccac57
rogilmore <rog1@psu.edu>
2020-07-06 15:21:21 -0400

    Version 0.2.3

--- R/check_databrary_ssl_certs.R
+++ R/check_databrary_ssl_certs.R
@@ -1,17 +1,19 @@
 #' Checks the SSL certificates for nyu.databary.org.
 #' @param url Target URL. Defaults to 'nyu.databrary.org'.
 #' @examples
-#' check_databrary_ssl_certs()
+#' check_ssl_certs()
 #' @export
-check_databrary_ssl_certs <- function(url = "nyu.databrary.org") {
-  if (!is.character(url)) {
-    stop('URL must be character string.')
-  }
-  x <- openssl::download_ssl_cert("nyu.databrary.org")
+check_ssl_certs <- function(host = "nyu.databrary.org") {
+  stopifnot(is.character(host))
+  
+  message(paste0('Checking SSL certificates for host: ', host))
+  x <- openssl::download_ssl_cert(host)
   validity_dates <- lapply(x, `[[`, "validity")
   issuer <- lapply(x, `[[`, "issuer")
+  
   df <- data.frame(issuer = unlist(issuer),
                    start_date = unlist(lapply(validity_dates, '[[', 1)),
                    exp_date = unlist(lapply(validity_dates, '[[', 2)))
+  
   df
 }
