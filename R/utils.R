ef526be3276c0d6e4d0087e5ddbb20523277d24e
rogilmore <rog1@psu.edu>
2023-07-11 09:20:54 -0400

    Move several minor functions to utils.R

---
+++ R/utils.R
@@ -0,0 +1,162 @@
+# R/utils.R
+# 
+# Utility functions.
+
+#-------------------------------------------------------------------------------
+#' Get the duration (in ms) of a file.
+#'
+#' @param asset_id Asset number.
+#' @param types_w_durations Asset types that have valid durations.
+#' @param vb A Boolean value. If TRUE provides verbose output.
+#' @examples
+#' get_file_duration() # default is the test video from databrary.org/volume/1
+#' @export
+get_file_duration <- function(asset_id = 1,
+                              types_w_durations = c("-600", "-800"),
+                              vb = FALSE) {
+  # Test parameters---------------------------------------------------------
+  if (length(asset_id) > 1) {
+    stop("asset_id must have length == 1.")
+  }
+  if (!is.numeric(asset_id)) {
+    stop("asset_id must be numeric")
+  }
+  if (asset_id < 1) {
+    stop("asset_id must be >= 1")
+  }
+  if (length(vb) > 1) {
+    stop("vb must have length == 1.")
+  }
+  if (!is.logical(vb)) {
+    stop("vb must be logical")
+  }
+  
+  r <-
+    GET_db_contents(URL_components = paste0('/api/asset/', asset_id),
+                    vb = vb)
+  if (r$format %in% types_w_durations) {
+    r$duration
+  } else {
+    if (vb)
+      message("File type does not have a defined duration.")
+    return(NULL)
+  }
+}
+
+#-------------------------------------------------------------------------------
+#' Get the time range from a given video or audio asset.
+#'
+#' @param vol_id Volume ID
+#' @param session_id Slot/session number.
+#' @param asset_id Asset number.
+#' @param convert_JSON A Boolean value. If TRUE, convert JSON to a data frame. Default is TRUE.
+#' @param segment_only A Boolean value. If TRUE, returns only the segment values. Otherwise returns
+#' a data frame with two fields, segment and permission. Default is TRUE.
+#' @param vb A Boolean value. If TRUE provides verbose output.
+#' @examples
+#' get_asset_segment_range()
+#' @export
+get_asset_segment_range <- function(vol_id = 1,
+                                    session_id = 9807,
+                                    asset_id = 1,
+                                    convert_JSON = TRUE,
+                                    segment_only = TRUE,
+                                    vb = FALSE) {
+  # Test parameters--------------------------------------------------------
+  if (length(vol_id) > 1) {
+    stop("vol_id must have length == 1.")
+  }
+  if (!is.numeric(vol_id)) {
+    stop("vol_id must be numeric")
+  }
+  if (vol_id < 1) {
+    stop("vol_id must be >= 1")
+  }
+  if (length(session_id) > 1) {
+    stop("session_id must have length == 1.")
+  }
+  if (!is.numeric(session_id)) {
+    stop("session_id must be numeric")
+  }
+  if (length(asset_id) > 1) {
+    stop("asset_id must have length == 1.")
+  }
+  if (session_id < 1) {
+    stop("slot.id must be >= 1")
+  }
+  if (!is.numeric(asset_id)) {
+    stop("asset_id must be numeric")
+  }
+  if (asset_id < 1) {
+    stop("asset_id must be >= 1")
+  }
+  if (length(vb) > 1) {
+    stop("vb must have length == 1.")
+  }
+  if (!is.logical(vb)) {
+    stop("vb must be logical")
+  }
+  
+  r <-
+    GET_db_contents(
+      URL_components = paste0(
+        '/api/volume/',
+        vol_id,
+        "/slot/",
+        session_id,
+        "/asset/",
+        asset_id
+      ),
+      vb = vb,
+      convert_JSON = convert_JSON
+    )
+  if (vb) {
+    message(
+      "Returning segment start & end times (in ms) from volume ",
+      vol_id,
+      ", session ",
+      session_id,
+      ", asset ",
+      asset_id
+    )
+  }
+  if (segment_only) {
+    return(r$segment)
+  } else {
+    return(r)
+  }
+}
+
+#-------------------------------------------------------------------------------
+#' Extracts Databrary permission level codes and labels.
+#' @param vb A Boolean value. If TRUE provides verbose output.
+#' @examples
+#' get_permission_levels()
+#' @export
+get_permission_levels <- function(vb = FALSE) {
+  c <- assign_constants(vb = vb)
+  pl <- c$permission
+  return(pl)
+}
+
+#-------------------------------------------------------------------------------
+#' Get the time range from a given video or audio asset.
+#'
+#' @param HHMMSSmmm a string in the format "HH:MM:SS:mmm"
+#' @examples
+#' HHMMSSmmm_to_ms()
+#' @export
+HHMMSSmmm_to_ms <- function(HHMMSSmmm = "01:01:01:333") {
+  # Check parameters
+  if (!is.character(HHMMSSmmm)) {
+    stop("HHMMSSmmm must be a string.")
+  }
+  
+  if (stringr::str_detect(HHMMSSmmm, "([0-9]{2}):([0-9]{2}):([0-9]{2}):([0-9]{3})")) {
+    time_segs <- stringr::str_match(HHMMSSmmm, "([0-9]{2}):([0-9]{2}):([0-9]{2}):([0-9]{3})")
+    as.numeric(time_segs[5]) + as.numeric(time_segs[4])*1000 + as.numeric(time_segs[3])*1000*60 +
+      as.numeric(time_segs[2])*1000*60*60
+  } else {
+    NULL
+  }
+}
