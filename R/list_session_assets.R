82353da9e5d44dfe409c309a30675f43e2e03ba4
rogilmore <rog1@psu.edu>
2024-01-26 12:16:12 -0500

    Clean-up list_session_assets.R; create new download_session_asset.R.

--- R/list_session_assets.R
+++ R/list_session_assets.R
@@ -38,34 +38,37 @@ list_session_assets <-
     }
     
     # Select session info
-    these_sessions <- purrr::map(vol_list$containers, get_sessions) |>
+    these_sessions <-
+      purrr::map(vol_list$containers, get_sessions) |>
       purrr::list_rbind()
     
     session_match <- (session_id == these_sessions$session_id)
     if (sum(session_match) == 0) {
-      if (vb) message("No matching session_id: ", session_id)
+      if (vb)
+        message("No matching session_id: ", session_id)
       return(NULL)
     }
     session_match_index <- seq_along(session_match)[session_match]
     
     this_session <- vol_list$containers[[session_match_index]]
-    if (is.null(this_session)) return(NULL)
+    if (is.null(this_session))
+      return(NULL)
     
     assets_df <-
-      purrr::map(vol_list$containers[[session_match_index]]$assets, as.data.frame) |>
+      purrr::map(this_session$assets, as.data.frame) |>
       purrr::list_rbind()
-      
+    
     # ignore empty sessions
     if (dim(assets_df)[1] == 0)
       return(NULL)
-
+    
     if (!('size' %in% names(assets_df)))
       assets_df$size = NA
     if (!('duration' %in% names(assets_df)))
       assets_df$duration = NA
     if (!('name' %in% names(assets_df)))
       assets_df$name = NA
-
+    
     assets_df <- assets_df |>
       dplyr::select(id, format, duration, name, permission, size) |>
       dplyr::rename(
@@ -77,18 +80,28 @@ list_session_assets <-
         asset_size = size
       )
     
+    # Gather asset format info
     asset_formats_df <- list_asset_formats(vb = vb) |>
       dplyr::select(format_id, format_mimetype, format_extension, format_name)
-
-    dplyr::left_join(assets_df, asset_formats_df, by = dplyr::join_by(asset_format_id == format_id))
+    
+    # Join assets with asset format info
+    out_df <- dplyr::left_join(assets_df,
+                               asset_formats_df,
+                               by = dplyr::join_by(asset_format_id == format_id))
+    
+    # TODO: Decide whether to add session data, possibly as parameter
+    # out_df <- out_df |>
+    #   dplyr::mutate(
+    #     session_id = this_session$id,
+    #     session_name = this_session$name,
+    #     session_date = this_session$date,
+    #     session_release = this_session$release
+    #   )
+    out_df
   }
 
 #-------------------------------------------------------------------------------
 #' Helper function for list_session_assets
 get_sessions <- function(volume_container) {
-  session_df <- tibble::tibble(session_id = volume_container$id,
-                               session_name =
-                                 volume_container$name)
-  
-  session_df
+  tibble::tibble(session_id = volume_container$id)
 }
