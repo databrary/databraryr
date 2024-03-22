00bd226652768a588485952ddbb096cb2e144a3c
rogilmore <rog1@psu.edu>
2024-03-22 08:20:16 -0400

    Minor edits to vignettes; change old list_session_assets() to list_volume_session() to reflect need for volume ID and session ID; rename list_session_assets2 to list_session_assets().

--- R/list_session_assets.R
+++ R/list_session_assets.R
@@ -1,123 +1,113 @@
-#' List Assets in a Session from a Databrary volume.
+#' List Assets in a Databrary Session.
+#' 
+#' #' @description
+#' `r lifecycle::badge("experimental")`
+#' 
+#' As of v0.6.3 `list_session_assets()` replaces an older function that is now
+#' named `list_volume_session_assets()`.  The older function requires both
+#' a volume ID and a session ID. The new function requires only a session ID.
 #'
-#' @param vol_id Target volume number.
-#' @param session_id The session number in the selected volume.
-#' @param vb A boolean value.
-#' @param rq An `httr2` request object.
+#' @param session_id An integer. A Databrary session number. Default is 9807,
+#' the "materials" folder from Databrary volume 1.
+#' @param vb A logical value. Show verbose feedback. Default is FALSE.
+#' @param rq An `httr2` request object. If NULL, a default request is generated
+#' from databraryr::make_default_request().
 #' 
 #' @returns A data frame with information about all assets in a volume.
 #' 
 #' @examples
 #' \donttest{
 #' \dontrun{
-#' list_session_assets() # Session 9807 in volume 1
+#' list_session_assets_2() # Session 9807 in volume 1
 #' }
 #' }
 #' @export
-list_session_assets <-
-  function(vol_id = 1,
-           session_id = 9807,
-           vb = FALSE,
-           rq = NULL) {
-    # Check parameters
-    assertthat::assert_that(length(vol_id) == 1)
-    assertthat::assert_that(is.numeric(vol_id))
-    assertthat::assert_that(vol_id >= 1)
-    
-    assertthat::assert_that(length(session_id) == 1)
-    assertthat::assert_that(is.numeric(session_id))
-    assertthat::assert_that(session_id >= 1)
-    
-    assertthat::assert_that(length(vb) == 1)
-    assertthat::assert_that(is.logical(vb))
-    
-    assertthat::assert_that(is.null(rq) |
-                              ("httr2_request" %in% class(rq)))
-    if (is.null(rq)) {
-      if (vb) {
-        message("NULL request object. Will generate default.")
-        message("Not logged in. Only public information will be returned.")  
-      }
-     rq <- databraryr::make_default_request()
+list_session_assets <- function(session_id = 9807,
+                                  vb = FALSE,
+                                  rq = NULL) {
+  assertthat::assert_that(length(session_id) == 1)
+  assertthat::assert_that(is.numeric(session_id))
+  assertthat::assert_that(session_id >= 1)
+  
+  assertthat::assert_that(is.null(rq) |
+                            ("httr2_request" %in% class(rq)))
+  
+  # Handle NULL rq
+  if (is.null(rq)) {
+    if (vb) {
+      message("NULL request object. Will generate default.")
+      message("Not logged in. Only public information will be returned.")
     }
-    
-    vol_list <- databraryr::get_volume_by_id(vol_id, vb, rq)
-    
-    if (!("containers" %in% names(vol_list))) {
-      if (vb)
-        message("No session/containers data from volume ", vol_id)
-      return(NULL)
-    }
-    
-    #--------------------------------------------------------------------------
-    get_sessions <- function(volume_container) {
-      tibble::tibble(session_id = volume_container$id)
-    }
-    #--------------------------------------------------------------------------
-    
-    # Select session info
-    these_sessions <-
-      purrr::map(vol_list$containers, get_sessions) %>%
-      purrr::list_rbind()
-    
-    session_match <- (session_id == these_sessions$session_id)
-    if (sum(session_match) == 0) {
-      if (vb)
-        message("No matching session_id: ", session_id)
-      return(NULL)
-    }
-    session_match_index <- seq_along(session_match)[session_match]
-    
-    this_session <- vol_list$containers[[session_match_index]]
-    if (is.null(this_session))
-      return(NULL)
-    
-    assets_df <-
-      purrr::map(this_session$assets, as.data.frame) %>%
-      purrr::list_rbind()
-    
-    # ignore empty sessions
-    if (dim(assets_df)[1] == 0)
-      return(NULL)
-    
-    if (!('size' %in% names(assets_df)))
-      assets_df$size = NA
-    if (!('duration' %in% names(assets_df)))
-      assets_df$duration = NA
-    if (!('name' %in% names(assets_df)))
-      assets_df$name = NA
-    
-    id <- NULL
-    format <- NULL
-    name <- NULL
-    duration <- NULL
-    permission <- NULL
-    size <- NULL
-    asset_format_id <- NULL
-    
-    assets_df <- assets_df %>%
-      dplyr::select(id, format, duration, name, permission, size) %>%
-      dplyr::rename(
-        asset_id = id,
-        asset_format_id = format,
-        asset_name = name,
-        asset_duration = duration,
-        asset_permission = permission,
-        asset_size = size
-      )
-    
-    format_id <- NULL
-    format_mimetype <- NULL
-    format_extension <- NULL
-    format_name <- NULL
-    
-    # Gather asset format info
-    asset_formats_df <- list_asset_formats(vb = vb) %>%
-      dplyr::select(format_id, format_mimetype, format_extension, format_name)
-    
-    # Join assets with asset format info
-    out_df <- dplyr::left_join(assets_df,
-                               asset_formats_df,
-                               by = dplyr::join_by(asset_format_id == format_id))
-    out_df
+    rq <- databraryr::make_default_request()
   }
+  
+  this_rq <- rq %>%
+    httr2::req_url(sprintf(QUERY_SLOT, session_id)) %>%
+    httr2::req_progress()
+  
+  resp <- tryCatch(
+    httr2::req_perform(this_rq),
+    httr2_error = function(cnd)
+      NULL
+  )
+  
+  if (!is.null(resp)) {
+    session_list <- httr2::resp_body_json(resp)
+    if ("assets" %in% names(session_list)) {
+      assets_df <- purrr::map(session_list$assets, as.data.frame) %>%
+        purrr::list_rbind()
+      
+      # ignore empty sessions
+      if (dim(assets_df)[1] == 0)
+        return(NULL)
+      
+      if (!('size' %in% names(assets_df)))
+        assets_df$size = NA
+      if (!('duration' %in% names(assets_df)))
+        assets_df$duration = NA
+      if (!('name' %in% names(assets_df)))
+        assets_df$name = NA
+      
+      id <- NULL
+      format <- NULL
+      name <- NULL
+      duration <- NULL
+      permission <- NULL
+      size <- NULL
+      asset_format_id <- NULL
+      
+      assets_df <- assets_df %>%
+        dplyr::select(id, format, duration, name, permission, size) %>%
+        dplyr::rename(
+          asset_id = id,
+          asset_format_id = format,
+          asset_name = name,
+          asset_duration = duration,
+          asset_permission = permission,
+          asset_size = size
+        )
+      
+      format_id <- NULL
+      format_mimetype <- NULL
+      format_extension <- NULL
+      format_name <- NULL
+      
+      # Gather asset format info
+      asset_formats_df <- list_asset_formats(vb = vb) %>%
+        dplyr::select(format_id, format_mimetype, format_extension, format_name)
+      
+      # Join assets with asset format info
+      out_df <- dplyr::left_join(assets_df,
+                                 asset_formats_df,
+                                 by = dplyr::join_by(asset_format_id == format_id)) %>%
+        dplyr::mutate(session_id = session_id)
+      
+      out_df
+    } else {
+      if (vb) message("No assets for session_id ", session_id)
+      session_list
+    }
+  } else {
+    resp
+  }
+}
